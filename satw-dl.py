14dd84fa788c1ada2d8a688749c442375fcb7824
Karn Wong <22489525+kahnwong@users.noreply.github.com>
2017-10-25 04:00:00 +0700

    Snapshot

--- satw-dl.py
+++ satw-dl.py
@@ -1,76 +1,123 @@
 import requests
 from bs4 import BeautifulSoup
-#~ import json
 from urllib.parse import urlparse
-import urllib.request
+import os
+from time import sleep
+from fake_useragent import UserAgent
+
+ua = UserAgent()
+header = {'User-Agent':str(ua.chrome)}
+
+class Scraper(object):
+    def __init__(self, name, url, pages, start):
+        self.name = name
+        self.url = url
+        self.pages = pages
+        self.start = start-1
+
+    def make_request(self, url):
+        response = requests.get(url, allow_redirects=True, headers=header).content
+        soup = BeautifulSoup(response, 'html.parser')
+        sleep(0.2)
+        return soup
+
+    def get_images(self):
+        raise NotImplementedError
+
+    def download(self):
+        images = self.get_images()
+
+        try:
+            os.mkdir(self.name)
+        except FileExistsError:
+            pass
+        print('======', self.name, '======')
+
+        for index, image in enumerate(images):
+            parsed = urlparse(image)
+            filename = parsed.path.split('/')[-1]
+
+            full_path = self.name + '/' + str(index), filename
+
+            r = requests.get(image)
+            with open(full_path, 'wb') as img_obj:
+                img_obj.write(r.content)
+                print(filename)
+
+        sleep(2)
+
+class SATW(Scraper):
+    def get_thumbnail(self):
+        thumbnails = []
+        for i in range(1, self.pages+1)[::-1]: # debug
+            page = self.url + 'page' + str(i)
+            print(page)
+
+            soup = self.make_request(page)
+
+            page_thumbnails = []
+            for scrape in soup.find_all("div", class_="col-xs-6 col-sm-2"):
+                thumbnail = [x.get("href") for x in scrape.find_all('a')]
+                page_thumbnails.extend(thumbnail)
+
+            page_thumbnails = page_thumbnails[::-1]
+            # print(page_thumbnails)
+            thumbnails.extend(page_thumbnails)
+
+        return thumbnails
+
+    def get_images(self): # plus description
+        thumbnails = self.get_thumbnail()
+
+        images = []
+        descriptions = []
+        start = self.start
+
+        for i in thumbnails[start:]: # debug
+            soup = self.make_request(i)
+
+            # get comic url
+            comic = [i.get('src') for i in soup.select('center > a > img')][1]
+            print(comic)
+            images.append(comic)
+
+            # dump description to html
+            description = [i for i in soup.find_all('div', class_='col-md-9')][0]
+            descriptions.append(description)
+
+        return images, descriptions
+
+    def download(self):
+        images, descriptions = self.get_images()
+
+        try:
+            os.mkdir(self.name)
+        except FileExistsError:
+            pass
+        print('======', self.name, '======')
+
+        for index, (image, description) in enumerate(zip(images, descriptions), self.start):
+            parsed = urlparse(image)
+            filename = parsed.path.split('/')[-1]
+
+            full_path = self.name + '/' + str(index+1) + ' ' + filename
+
+            r = requests.get(image)
+            with open(full_path, 'wb') as img_obj:
+                img_obj.write(r.content)
+                print(index+1, filename)
+
+            description_filename = full_path + '.html'
+            with open(description_filename, 'w') as f:
+                f.write(description.prettify())
+
+        sleep(2)
 
 
-def satw_dl(url, max_pages):
-	
-	"""Pages"""
-	
-	urls = []
-	
-	urls.append(url)
-	for page in list(range(2, max_pages+1)):
-		urls.append(url + 'page' + str(page))
-	#~ print(urls[1:])
-	
-	"""Clean up"""
-	
-	links = []
-	
-	for url in urls:
-		
-		opener = urllib.request.build_opener()
-		opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-		response = opener.open(url)
-		page = response.read()
-		soup = BeautifulSoup(page,'html5lib')
-		
-		for scrape in soup.find_all("div", class_="col-xs-6 col-sm-2"):
-			full_link = [x.get("href") for x in scrape.find_all('a')]
-			links.append(full_link[0])
-			print(full_link)
-	
-	"""Extract comic urls"""
-	
-	actual_comics = []
-	
-	for link in links:
+def main(site):
+    if site == 'satw':
+        i = SATW('SATW', 'https://satwcomic.com/the-world/', 12, 1)
+        i.download()
 
-		opener = urllib.request.build_opener()
-		opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-		response = opener.open(link)
-		page = response.read()
-		soup = BeautifulSoup(page,'html5lib')
-		
-		comics = []
-		
-		for comic_link in soup.select('center > a > img'):
-			comic = comic_link.get('src')
-			comics.append(comic)
-			print(comic)
-		actual_comics.append(comics[1])
-	actual_comics = actual_comics[::-1]
-	
-	#~ """Dump urls"""
-	#~ with open ('satw.json', 'w') as file_object:
-		#~ json.dump(actual_comics, file_object)
-		#~ print('satw.json', 'exported!')
-		
-			
-	"""Download comics"""	#indexed	
-				
-	for index, comic in enumerate(actual_comics): 
-		
-		parsed = urlparse(comic)
-		filename = parsed.path.split('/')[-1]
-		image_link = requests.get(comic)
 
-		with open(str(index) + ' ' + filename, 'wb') as img_obj:
-			img_obj.write(image_link.content)
-
-		print("Download " + filename + " completed!")
-
-satw_dl('https://satwcomic.com/the-world/', 12)
+main('satw')
